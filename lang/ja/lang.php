1fa88f3e60e474c63e24c76f55d9df55ae725d10
Hideaki SAWADA <chuno@live.jp>
2014-05-24 03:25:08 +0200

    translation update

---
+++ lang/ja/lang.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author Hideaki SAWADA <chuno@live.jp>
+ */
+$lang['btn_create_new_page']   = '新規ページ作成';
+$lang['btn_create_page']       = 'ページ作成';
+$lang['btn_new_folder']        = '新規フォルダー';
+$lang['btn_ok']                = 'OK';
+$lang['dlg_new_folder']        = '新規フォルダーを入力：';
+$lang['msc_page_title']        = 'ページ見出し';
