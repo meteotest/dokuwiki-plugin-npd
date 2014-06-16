ebfd36af095c5060dc72d4869b87c63c2b7ae19b
christian studer <cstuder@existenz.ch>
2014-06-16 16:10:34 +0200

    Adds de-informal localization.

---
+++ lang/de-informal/lang.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Informal german language file for the "New Page Dialog" plugin
+ *
+ * Identical to the 'de' localization.
+ *
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * @author     Tilman Blumenbach <tilman [AT] tblue [DOT] de>
+ */
+
+/* buttons */
+$lang['btn_create_new_page'] = 'Neue Seite anlegen';
+$lang['btn_create_page'] = 'Seite anlegen';
+$lang['btn_new_folder']  = 'Neuer Ordner';
+$lang['btn_ok'] = 'OK';
+
+/* dialogs */
+$lang['dlg_new_folder'] = 'Namen für neuen Ordner eingeben:';
+
+/* misc */
+$lang['msc_page_title'] = 'Seitentitel';
