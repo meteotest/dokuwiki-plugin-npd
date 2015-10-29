49fa991fe32a1c428ebc4d2825ee4df2c9f99d43
Neli Dimitrova <neli.dimitrova.office@gmail.com>
2015-10-29 13:35:05 +0100

    translation update

--- lang/en/lang.php
+++ lang/en/lang.php
@@ -1,21 +1,14 @@
 <?php
+
 /**
- * english language file for the "New Page Dialog" plugin
- *
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
- * @author     Pierre Spring <pierre.spring[curly thing]liip[full stopW]ch>
+ * 
+ * @author Pierre Spring <pierre.spring[curly thing]liip[full stopW]ch>
+ * @author Neli Dimitrova <neli.dimitrova.office@gmail.com>
  */
-
-/* buttons */
-$lang['btn_create_new_page'] = 'Create New Page';
-$lang['btn_create_page'] = 'Create Page';
-$lang['btn_new_folder']  = 'New Folder';
-$lang['btn_ok'] = 'OK';
-
-/* dialogs */
-$lang['dlg_new_folder'] = 'Enter new folder name:';
-
-/* misc */
-$lang['msc_page_title'] = 'Page Title';
-
-//Setup VIM: ex: et ts=4 :
+$lang['btn_create_new_page']   = 'Създай нова страница';
+$lang['btn_create_page']       = 'Създай страница';
+$lang['btn_new_folder']        = 'Нова папка';
+$lang['btn_ok']                = 'Добре';
+$lang['dlg_new_folder']        = 'Въведете ново име на папката:';
+$lang['msc_page_title']        = 'Заглавие на страницата';
