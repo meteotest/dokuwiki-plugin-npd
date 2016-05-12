6d89a8782ac3951f3375e5cd928f5da7c0c814da
Rene <rene@re-gister.com>
2016-05-12 16:50:05 +0200

    translation update

--- lang/de/lang.php
+++ lang/de/lang.php
@@ -1,19 +1,13 @@
 <?php
+
 /**
- * german language file for the "New Page Dialog" plugin
- *
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
- * @author     Tilman Blumenbach <tilman [AT] tblue [DOT] de>
+ * 
+ * @author Tilman Blumenbach <tilman [AT] tblue [DOT] de>
  */
-
-/* buttons */
-$lang['btn_create_new_page'] = 'Neue Seite anlegen';
-$lang['btn_create_page'] = 'Seite anlegen';
-$lang['btn_new_folder']  = 'Neuer Ordner';
-$lang['btn_ok'] = 'OK';
-
-/* dialogs */
-$lang['dlg_new_folder'] = 'Namen für neuen Ordner eingeben:';
-
-/* misc */
-$lang['msc_page_title'] = 'Seitentitel';
+$lang['btn_create_new_page']   = 'Neue Seite anlegen';
+$lang['btn_create_page']       = 'Seite anlegen';
+$lang['btn_new_folder']        = 'Neuer Ordner';
+$lang['btn_ok']                = 'OK';
+$lang['dlg_new_folder']        = 'Namen für neuen Ordner eingeben:';
+$lang['msc_page_title']        = 'Seitentitel';
