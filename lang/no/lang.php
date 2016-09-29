bd7896dc57426191e683a32cd3bf3aa402e5856f
Arne Hanssen <arne.hanssne@getmail.no>
2016-09-29 09:15:41 +0200

    translation update

--- lang/no/lang.php
+++ lang/no/lang.php
@@ -1,19 +1,14 @@
 <?php
+
 /**
- * norsk språkfil for plugin "New Page Dialog"
- *
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
- * @author     Torkill Bruland
+ *
+ * @author Torkill Bruland
+ * @author Arne Hanssen <arne.hanssne@getmail.no>
  */
- 
-/* buttons */
-$lang['btn_create_new_page'] = 'Opprett Ny Side';
-$lang['btn_create_page'] = 'Lag side';
-$lang['btn_new_folder']  = 'Ny mappe';
-$lang['btn_ok'] = 'OK';
- 
-/* dialogs */
-$lang['dlg_new_folder'] = 'Før inn nytt mappenavn:';
- 
-/* misc */
-$lang['msc_page_title'] = 'Sidetittel';
+$lang['btn_create_new_page']   = 'Opprett Ny Side';
+$lang['btn_create_page']       = 'Lag side';
+$lang['btn_new_folder']        = 'Ny mappe';
+$lang['btn_ok']                = 'OK';
+$lang['dlg_new_folder']        = 'Skriv inn nytt mappenavn:';
+$lang['msc_page_title']        = 'Sidetittel';
